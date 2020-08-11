19290f1c49ed1ba166d33c73fdb3317ea1a1732c
Durant van Rensburg <durantvr18@gmail.com>
2020-08-11 17:53:35 -0400

    added smooth scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -143,3 +143,29 @@
 			});
 
 })(jQuery);
+
+//On ready
+$(document).ready(function () {
+    // Adds smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Makes sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevents default anchor click behavior
+            event.preventDefault();
+
+            // Stores hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Adds hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        }
+    });
+});
